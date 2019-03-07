b944dc45d4da172efa252f42a01284aadcc08849
Luca Lianas <luca.lianas@crs4.it>
2019-03-07 16:17:42 +0100

    bug fix

--- 84-setup-default-group.sh
+++ 84-setup-default-group.sh
@@ -6,7 +6,7 @@ export PATH="/opt/omero/web/venv/bin:$PATH"
 python=/opt/omero/web/venv/bin/python
 omero=/opt/omero/web/OMERO.web/bin/omero
 
-DEFAULT_OME_GROUP="{$DEFAULT_OME_GROUP:-}"
+DEFAULT_OME_GROUP="${DEFAULT_OME_GROUP:-}"
 
 if [ -n "$DEFAULT_OME_GROUP" ]; then
     $python $omero config set omero.web.ome_seadragon.search.default_group "$DEFAULT_OME_GROUP"
