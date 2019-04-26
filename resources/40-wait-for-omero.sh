ec39233605d2b1654a719fcaebac1110e1d9b1f9
Luca Lianas <luca.lianas@crs4.it>
2019-04-26 10:54:06 +0200

    minor fix

--- resources/40-wait-for-omero.sh
+++ resources/40-wait-for-omero.sh
@@ -11,7 +11,7 @@ OMEROHOST="${OMEROHOST:-}"
 OMEROPORT="${OMEROPORT:-4064}"
 
 # wait until OMERO.server accepts connections
-echo 'Checking OMERO.server at "$OMEROHOST"'
+echo "Checking OMERO.server $OMEROHOST"
 until $python $omero group list --server "$OMEROHOST" --port "$OMEROPORT" --user root --password "$ROOTPASS" > /dev/null 2>&1 ; do
     echo "OMERO.server not ready, waiting...";
     sleep 5
