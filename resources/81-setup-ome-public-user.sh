eb39cdda43dab3b302e9a5660f3eda38f7fbeecf
Luca Lianas <luca.lianas@crs4.it>
2019-06-19 16:55:20 +0200

    added the possibility to specify public user URL filter

--- resources/81-setup-ome-public-user.sh
+++ resources/81-setup-ome-public-user.sh
@@ -8,12 +8,13 @@ omero=/opt/omero/web/OMERO.web/bin/omero
 
 OME_PUBLIC_USER_NAME="${OME_PUBLIC_USER_NAME:-ome_public}"
 OME_PUBLIC_USER_PASS="${OME_PUBLIC_USER_PASS:-omero}"
+OME_PUBLIC_URL_FILTER="${OME_PUBLIC_URL_FILTER:-^/ome_seadragon}"
 
 echo "Setup OMERO public user"
 $python $omero config set omero.web.public.enabled True
 $python $omero config set omero.web.public.user "$OME_PUBLIC_USER_NAME"
 $python $omero config set omero.web.public.password "$OME_PUBLIC_USER_PASS"
-$python $omero config set omero.web.public.url_filter '^/ome_seadragon'
+$python $omero config set omero.web.public.url_filter "$OME_PUBLIC_URL_FILTER"
 $python $omero config set omero.web.public.server_id 1
 
 $python $omero config set omero.web.ome_seadragon.ome_public_user "$OME_PUBLIC_USER_NAME"
