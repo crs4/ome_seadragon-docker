41d13f10a608a57fcd7f3a7949630c7d7be9d272
Luca Lianas <luca.lianas@crs4.it>
2021-05-07 11:16:02 +0200

    added env variable to configure default MIRAX folder

--- resources/83-setup-images-repository.sh
+++ resources/83-setup-images-repository.sh
@@ -8,8 +8,13 @@ omero=/opt/omero/web/venv3/bin/omero
 
 IMAGES_REPOSITORY="${IMAGES_REPOSITORY:-/OMERO/}"
 IMAGES_FOLDER="${IMAGES_FOLDER:-}"
+MIRAX_FOLDER="${MIRAX_FOLDER:-}"
 
 $python $omero config set omero.web.ome_seadragon.repository "$IMAGES_REPOSITORY"
 if [ -n "$IMAGES_FOLDER" ]; then
     $python $omero config set omero.web.ome_seadragon.images_folder "$IMAGES_FOLDER"
+fi
+
+if [ -n "$MIRAX_FOLDER" ]; then
+    $python $omero config set omero.web.ome_seadragon.default_mirax_folder "$MIRAX_FOLDER"
 fi
